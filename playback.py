0505036d025af9d20afaa7e419ef121d05ba8278
Victoria Gallo <164115980+VictoriaGallo@users.noreply.github.com>
2025-10-13 19:47:09 -0500

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,8 @@
+# Solicita al usuario una entrada
+entrada = input("Ingrese una frase: ")
+
+# Reemplaza cada espacio con tres puntos
+salida = entrada.replace(" ", "...")
+
+# Muestra el resultado
+print(salida)
